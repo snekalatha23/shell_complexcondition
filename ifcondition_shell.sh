b950fb0e0392a7853ecca8f82aa8a1174bff83d9
snekalatha23 <57349438+snekalatha23@users.noreply.github.com>
2020-01-16 14:42:48 +0530

    Update ifcondition_shell.sh

--- ifcondition_shell.sh
+++ ifcondition_shell.sh
@@ -1,3 +1,4 @@
 if [ "x$(sed -re '/^[  ]?*[a-zA-Z0-9_]+=.*/{d};/[  ]?*#.*/{d}' $1)" = "x" ]; then
 echo "hello"
+echo "SELECT item_nbr, movement_status, storage_location FROM Received_Items where movement_status = 'pending'" | mysql $database -h $HOST -u $userid -p $password > $filename
 fi
